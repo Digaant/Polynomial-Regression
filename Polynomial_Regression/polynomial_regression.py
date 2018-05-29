1b4d4d938e0257b1886f5648c259a182d7a4b893
Digaant Garg <digaant.garg@gmail.com>
2018-05-29 11:05:46 +0530

    Update polynomial_regression.py

--- Polynomial_Regression/polynomial_regression.py
+++ Polynomial_Regression/polynomial_regression.py
@@ -28,10 +28,7 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
-#Fitting Linear regression to the dataset
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X,y)
+
 
 #Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
@@ -40,13 +37,6 @@ X_poly = poly_reg.fit_transform(X)
 lin_reg1 = LinearRegression()
 lin_reg1.fit(X_poly,y)
 
-#Visualising the Linear Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
-plt.title("Truth or Bluff(Linear Regression)")
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
-plt.show()
 #Visualising the Polynomial Regression results
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
